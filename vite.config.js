09cef492f5e674cbd4972be4ac2a945887c8d8b0
justin-care <care.justin@gmail.com>
2025-06-02 12:05:33 -0700

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    host: true
-  }
+    base: '/moxvault-basic/',
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 5173,
+        host: true
+    }
 })
